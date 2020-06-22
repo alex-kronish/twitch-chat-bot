dd3650b283f8460f5a9cb0bb482280b645a4d827
alex-kronish <alexkronish@gmail.com>
2020-06-21 22:45:47 -0400

    Hydrate working! have to test it while the stream is online still.

--- helpers.py
+++ helpers.py
@@ -1,22 +1,89 @@
 import requests
 import json
-import os
+import datetime
+
 
 # helper functions to keep the main.py file relatively clean.
 
+def checkzipcode(zipcode):
+    res = True
+    if len(zipcode) != 5:
+        res = False
+    else:
+        try:
+            int(zipcode)
+        except:
+            res = False
+    return res
+
 
 def weather(zipcode, config):
-    api_url = "https://api.openweathermap.org/data/2.5/weather?zip=" + \
-              zipcode + "&units=imperial&appid=" + \
-              config["openweathermap_api_key"]
-    req = requests.get(api_url)
-    status = req.status_code
-    if status != 200:
-        s = "Sorry, something went wrong with the OpenWeatherMap API."
+    if not checkzipcode(zipcode):
+        s = "Bad zip code entered: " + zipcode
     else:
-        res = req.json()
-        city = res["name"]
-        temp = str(res["main"]["temp"]) + " F"
-        desc = res["weather"][0]["description"]
-        s = "The weather in " + city + " is " + desc + " with a temperature of " + temp + "."
+        api_url = "https://api.openweathermap.org/data/2.5/weather?zip=" + \
+                  zipcode + "&units=imperial&appid=" + \
+                  config["openweathermap_api_key"]
+        req = requests.get(api_url)
+        status = req.status_code
+        if status != 200:
+            s = "Sorry, something went wrong with the OpenWeatherMap API."
+        else:
+            res = req.json()
+            city = res["name"]
+            temp = str(res["main"]["temp"]) + " F"
+            desc = res["weather"][0]["description"]
+            s = "The weather in " + city + " is " + desc + " with a temperature of " + temp + "."
+    return s
+
+
+def twitchbearertoken(config):
+    api_url = "https://id.twitch.tv/oauth2/token?client_id=" + config["twitch_client_id"] + "&client_secret=" \
+              + config["twitch_client_secret"] + "&grant_type=client_credentials"
+    tokenreq = requests.post(api_url)
+    if tokenreq.status_code != 200:
+        return "None"
+    else:
+        tokendict = tokenreq.json()
+        bearer_token = tokendict["access_token"]
+        return bearer_token
+
+
+def hydrate(config):
+    # step 1: figure out how long stream has been online
+    # this requires two api calls, one to get the auth token and another to actually get the data we need
+    token = twitchbearertoken(config)
+    s = ""
+    if token == "None":
+        s = "Couldn't connect to the Twitch Helix API to get a token."
+    else:
+        client_id = config["twitch_client_id"]
+        h = {
+            "Authorization": "Bearer " + token,
+            "Client-ID": client_id
+        }
+        api_url = "https://api.twitch.tv/helix/streams?user_login=" + config["twitch_initial_channels"][0]
+        streamdata = requests.get(api_url, headers=h)
+        if streamdata.status_code != 200:
+            s = "Something went wrong with the API Call. HTTP Status Code: " + str(streamdata.status_code)
+        else:
+            if len(streamdata.json()["data"]) == 0:
+                s = "The stream is offline. You may uncontrollably guzzle cum at your " \
+                    "convenience. Stay hydrated, gamers :)"
+            else:
+                stream = streamdata.json()["data"][0]
+                time_str = stream["started_at"]
+                started_time = datetime.datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+                print(str(started_time))
+                current_time = datetime.datetime.utcnow()
+                print(str(current_time))
+                timedelta = current_time - started_time
+                # calculate how much cum i should have ingested by this point.
+                daily_cum_intake = 3.7  # liters
+                cum_intake_per_sec = daily_cum_intake / 86400.0  # number of seconds per day
+                sec_total = timedelta.seconds
+                cum_intake_recommended = sec_total * cum_intake_per_sec
+                live_time = str(timedelta)
+                s = "The stream has been going for " + live_time + ". Mico should have guzzled about " + \
+                    str(cum_intake_recommended) + " Liters of cum so far. Stay hydrated, gamers :)"
     return s
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import requests
-import twitchio
 from time import sleep
 from twitchio.ext import commands
 import json
-import os
 import helpers
 
 # Pull in the config json file and parse it into a dict
@@ -29,7 +26,7 @@ async def event_ready():
     await ws.send_privmsg(config["twitch_initial_channels"][0], f"/me has Logged In. Howdy gamers.")
 
 
-@twbot.command(name="bothelp")
+@twbot.command(name="h")
 async def bothelp(ctx):
     # print(str(ctx.content))
     await ctx.send("*** Testing.")
@@ -43,11 +40,18 @@ async def weather(ctx):
     if len(ziparray) <= 1:
         await ctx.send("Not enough arguments. Syntax: !weather zip, like this: !weather 07801")
         sleep(1.5)
-        zipcd = "10001"
+        zipcd = "10001"  # NYC
     else:
         zipcd = ziparray[1]
     s = helpers.weather(zipcd, config)
     await ctx.send(s)
+
+
+@twbot.command(name="hydrate")
+async def hydrate(ctx):
+    s = helpers.hydrate(config)
+    await ctx.send(s)
+
 
 # so that i can start up the bot by calling main.py with a cron job
 if __name__ == "__main__":

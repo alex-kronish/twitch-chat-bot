16deba57e4322a92e49830447973325cd0ed5556
alex-kronish <alexkronish@gmail.com>
2020-06-29 22:04:42 -0400

    Adding some features from the discord bot i'm also working on

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from time import sleep
 from twitchio.ext import commands
 import json
 import helpers
+import markov
+import random
 
 # Pull in the config json file and parse it into a dict
 config_txtfile = open("config/config_secret.json", "rt+")
@@ -16,6 +18,8 @@ twbot = commands.Bot(
     prefix=config["twitch_bot_prefix"],
     initial_channels=config["twitch_initial_channels"]
 )
+m = markov.MarkovChainer()
+helpers.markov_startup(m)
 
 
 # bot commands below this line
@@ -24,6 +28,25 @@ async def event_ready():
     print("bot ready......")
     ws = twbot._ws
     await ws.send_privmsg(config["twitch_initial_channels"][0], f"/me has Logged In. Howdy gamers.")
+
+
+@twbot.event
+async def event_message(ctx):
+    # runs every time a message is sent.
+    if ctx.author.name.lower() == config["twitch_username"].lower():
+        return
+    msg = ctx.content
+    action_calc = random.randrange(1, 50)
+    helpers.markov_add(m, msg, config["markov_excluded_words"])
+    s = None
+    if action_calc == 10:
+        s = helpers.butt_replace(msg)
+    elif action_calc == 20:
+        s = m.generate_sentence()
+    if s is None:
+        return
+    else:
+        await ctx.send(s)
 
 
 @twbot.command(name="h")
@@ -55,7 +78,8 @@ async def hydrate(ctx):
 
 @twbot.command(name="discord")
 async def discord(ctx):
-    s = "Well there isn't one but maybe one day."
+    s = "Well there isn't one, but maybe one day. Subscribe to grow the channel, maybe I'll be big enough to justify " \
+        "it! "
     await ctx.send(s)
 
 
